af69907e3faf2289df9ae0ab40123e428b03c714
Nguyen Tien Dat <tie.dat2004@gmail.com>
2026-06-18 16:13:46 +0700

    chore

--- log-monitor-frontend/src/api/axiosClient.js
+++ log-monitor-frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8082",
+    baseURL: import.meta.env.VITE_API_BASE_URL ?? "",
     timeout: 10_000,
     headers: {
         "Content-Type": "application/json",
